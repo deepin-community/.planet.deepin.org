b933fc8e57b955c8434dd8819f072d6da7d79185
Wang Zichong <wangzichong@deepin.org>
2023-01-29 14:53:58 +0800

    chore: remove setColorScheme() in js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,3 @@
-const setColorTheme = (theme) => {
-  document.documentElement.setAttribute('data-bs-theme', theme);
-  window.localStorage.setItem('colorScheme', theme);
-}
-
-const colorScheme = window.localStorage.getItem("colorScheme");
-if (colorScheme) {
-  setColorTheme(colorScheme);
-}
-
 const storeLang = (event) => {
   window.localStorage.setItem('lang', event.target.lang);
 }
